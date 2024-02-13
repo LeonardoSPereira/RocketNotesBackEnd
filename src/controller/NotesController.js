1f6849322a7400382452f8061c64133c17043fef
Leonardo Pereira <leonardo_human@hotmail.com>
2024-02-13 13:13:47 -0300

    Update: update dependencies version

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -82,6 +82,7 @@ class NotesController {
         }
 
         const userTags = await knex("tags").where({ user_id });
+        
         const notesWithTags = notes.map(note => {
             const noteTags = userTags.filter(tag => tag.note_id === note.id)
 
